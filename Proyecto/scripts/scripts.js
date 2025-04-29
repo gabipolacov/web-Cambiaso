1755b5aff131593bb542cfd2bc260a6bb42b592b
gabrielapolacov <gabrielamorenopolacov@gmail.com>
2025-04-28 21:17:13 -0300

    Finished basic search function on javascript whne clicking search button

--- Proyecto/scripts/scripts.js
+++ Proyecto/scripts/scripts.js
@@ -5,18 +5,21 @@ const products = [
         image: "./imagenes/Cortadoradecesped.webp",
         price: 29000,
         name: "Cortadora de Césped",
+        category:"Jardín",
         description: "¡Mantené tu jardín impecable con esta potente cortadora de césped! Funciona perfectamente, ideal para jardines medianos y grandes. Motor eficiente, cuchillas afiladas y fácil de manejar.",
     },
     {
         image: "./imagenes/tablet.png",
         price:100000,
         name: "Tablet Samsung",
+        category:"Electrónica",
         description: "¡Potencia y comodidad en tus manos! Esta tablet es ideal para estudiar, trabajar o disfrutar de tus series y juegos favoritos. Pantalla nítida, batería de larga duración y rendimiento fluido.",
     },
     {
         image: "./imagenes/Cocina.webp",
         price:530000,
         name: "Cocina",
+        category: "Electrodoméstico",
         description: "¡Renová tu cocina con este increíble modelo! Perfecta para preparar tus comidas favoritas con facilidad y eficiencia. Funciona a la perfección, con hornallas potentes y horno parejo.",
     },
 ];
@@ -41,6 +44,9 @@ function createProductCard(product) {
     const title = document.createElement('h3');
     title.textContent = product.name;
 
+    const category = document.createElement('h3');
+    category.textContent = product.category;
+
     const description = document.createElement('p');
     description.textContent = product.description;
 
@@ -51,6 +57,7 @@ function createProductCard(product) {
     card.appendChild(img);
     card.appendChild(title);
     card.appendChild(price);
+    card.appendChild(category);
     card.appendChild(description);
     card.appendChild(button);
 
@@ -58,11 +65,73 @@ function createProductCard(product) {
 }
 
 /*Se recorre el array de productos para generar cada tarjeta*/
-
 products.forEach( product => {
     const card = createProductCard(product);
     grid.appendChild(card);
 });
+
+/*-----------------------------------------------------------*/
+/*Agregar productos al gallery*/
+const form = document.getElementById('sell-form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const newProduct = { 
+        image: document.getElementById('image').value,
+        price: document.getElementById('price').value,
+        name: document.getElementById('title').value,
+        category: document.getElementById('category').value,
+        description: document.getElementById('description').value,
+    };
+
+    const card = createProductCard(newProduct);
+
+// Agrega la tarjeta al contenedor
+grid.appendChild(card);
+
+// Resetea el formulario
+form.reset();
+
+});
+
+/*Quitar productos del Carrito*/
+
+
+/*Agregar productos del Carrito*/
+
+/*Búsqueda*/
+const searchInput = document.querySelector('#search-product');
+const searchInputBtn = document.querySelector('#btn-search-product');
+
+function renderProducts(list){
+
+    list.forEach( product => {
+        const card = createProductCard(product);
+        grid.appendChild(card);
+    });
+}
+
+function filterProducts(text){
+    console.log('La función filterProducts se está ejecutando');
+
+
+    const filteredProducts = products.filter( product => {
+        return product.name.toLowerCase().includes(text.toLowerCase());
+    });
+    grid.innerHTML = '';
+    renderProducts(filteredProducts);
+}
+
+    searchInputBtn.addEventListener('click', (e) => {
+        e.preventDefault(); // <- esto evita el refresh del formulario
+        console.log('evento click detectado');
+        const valorInput = searchInput.value;
+
+        filterProducts(valorInput);
+    });
+
+//renderProducts(products);
 
 /*Codigo para desplegar menú en pantallas chicas*/
 const menuToggle = document.querySelector('.menu-toggle');
