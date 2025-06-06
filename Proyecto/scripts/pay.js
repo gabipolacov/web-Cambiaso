ae08bde42b772602c966e26b976c6927aa98a1ac
gabrielapolacov <gabrielamorenopolacov@gmail.com>
2025-06-05 21:04:02 -0300

    Added pay form js logic

---
+++ Proyecto/scripts/pay.js
@@ -0,0 +1,44 @@
+const API_TOKEN = 'patzuzJS60aaOG2eX.c5c086240d6bd5338c0e9bf4ba22c453eabc7f051ca170a1ed493976fc0ac8a2';
+const BASE_ID = 'apppfuJapye8WbhBo';
+const TABLE_NAME = 'Payments';
+const API_URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
+const form = document.getElementById('pay-data');
+
+//Función Para agregar productos a Airtable
+async function addProduct(product) {
+    const newProduct = {
+        fields: product
+    };
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newProduct)
+    });
+    const data = await response.json();
+    console.log(data);
+}
+
+//Evento para hacer submit del formulario
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const formPayment = {
+        name: document.getElementById('nombre').value,
+        lastname: document.getElementById('apellido').value,
+        address: document.getElementById('direccion').value,
+        province: document.getElementById('provincia').value,
+        city: document.getElementById('localidad').value,
+        cp: document.getElementById('CP').value,
+
+        method: document.getElementById('pago').value,
+        cardnumber: document.getElementById('numero').value,
+        CVV: document.getElementById('clave').value,
+        cardholder: document.getElementById('titular').value,
+        dni: document.getElementById('dni').value,
+    
+    };
+    await addProduct(formPayment);
+}); 
