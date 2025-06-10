dc5463142e020bb4ab71623f3f3835f1c959abf7
gabrielapolacov <gabrielamorenopolacov@gmail.com>
2025-06-09 22:46:07 -0300

    Trying to fix footer

--- Proyecto/scripts/details.js
+++ Proyecto/scripts/details.js
@@ -2,7 +2,6 @@
 const params = new URLSearchParams(window.location.search);
 const Id = parseInt(params.get('Id'));
 const detailSection = document.getElementById('details');
-
 const API_TOKEN = 'patzuzJS60aaOG2eX.c5c086240d6bd5338c0e9bf4ba22c453eabc7f051ca170a1ed493976fc0ac8a2';
 const BASE_ID = 'apppfuJapye8WbhBo';
 const TABLE_NAME = 'Products';
