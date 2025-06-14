827c962532af7599a97083d2bd56ff7c58ddf1b8
gabrielapolacov <gabrielamorenopolacov@gmail.com>
2025-06-14 19:27:16 -0300

    Finished Css of new screens

--- Proyecto/scripts/paycard.js
+++ Proyecto/scripts/paycard.js
@@ -1,7 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
 console.log('ID recibido desde URL:', id);
-const paymentCard = document.getElementById('card-content');
 
 const API_TOKEN = 'patzuzJS60aaOG2eX.c5c086240d6bd5338c0e9bf4ba22c453eabc7f051ca170a1ed493976fc0ac8a2';
 const BASE_ID = 'apppfuJapye8WbhBo';
