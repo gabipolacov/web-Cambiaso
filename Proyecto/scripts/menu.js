12cc9a0558d40fdea6a4e5047b365170addada15
gabrielapolacov <gabrielamorenopolacov@gmail.com>
2025-07-29 21:57:58 -0300

    Moví archivos a docs para GitHub Pages

--- Proyecto/scripts/menu.js
+++
@@ -1,7 +0,0 @@
-//Codigo para desplegar menú en pantallas chicas
-const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
