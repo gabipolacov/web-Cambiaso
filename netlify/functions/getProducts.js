423d45c544dbbe9d689e8e68c49af14b3941d4a7
gabrielapolacov <gabrielamorenopolacov@gmail.com>
2025-08-07 23:02:54 -0300

    Added serverless function to bring products from airtable

---
+++ netlify/functions/getProducts.js
@@ -0,0 +1,29 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function (event, context) {
+  const API_KEY = process.env.AIRTABLE_API_KEY;
+  const BASE_ID = "apppfuJapye8WbhBo"; // Reemplazá esto
+  const TABLE_NAME = "Products"; // Reemplazá si el nombre es distinto
+
+  const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
+
+  try {
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    });
+
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Hubo un error al obtener los datos" }),
+    };
+  }
+};
